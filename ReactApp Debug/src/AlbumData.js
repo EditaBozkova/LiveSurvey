8ad7ed40bdc8ce62782887541bdb0fb0949b52cb
EditaBozkova <edita.bozkova@seznam.cz>
2021-05-06 23:25:34 +0200

    JSON data anket

--- ReactApp Debug/src/AlbumData.js
+++ ReactApp Debug/src/AlbumData.js
@@ -1,123 +1,175 @@
 const JSONdata = [
     {
         "info": {
-                "SName": "Testovaci dotaznik", 
-                "SAuthor": "čet. Jitka Volná", 
-                "SAbout": "Dotazník testovani", 
-                "QuestSum": "15", 
+                "SName": "Kryptoměny", 
+                "SAuthor": "des. Edita Božková", 
+                "SAbout": "Průzkum napříč veřejností za účelem zjištění důvěry společnosti v jednotlivé kryptoměny.", 
+                "QuestSum": "14", 
                 "STime": "10 minut"
             },
-        "pages": [
+        "questions": [
             {
-                "question": [ {
-                    "type": "text",
-                    "QTitle": "Vaše hodnost?"
-                    }
-                ]
+                "Type": "radiogroup",
+                "CorrectAnsw": "false",
+                "QTitle": "Jaké je Vaše pohlaví",
+                "Choices": ["Žena", "Muž"]
             }, {
-                "question": [ {
-                    "type": "radiogroup",
-                    "QTitle": "Kdy jsou Vánoce?",
-                    "Choices": ["12.12.", "24.12.", "1.1."],
-                    "CorrectAnsw": "24.12."
-                    }
-                ]
+                "Type": "text",
+                "QTitle": "Kolik je Vám let?"
             }, {
-                "question": [ {
-                    "type": "checkbox",
-                    "QTitle": "Zvolte mužská jména",
-                    "Choices": ["Radek", "Aneta", "Pavel"],
-                    "CorrectAnsw": ["Radek", "Pavel"]
-                    }
-                ]
+                "Type": "radiogroup",
+                "CorrectAnsw": "false",
+                "QTitle": "Jaké je Vaše nejvyšší dosažené vzdělání?",
+                "Choices": ["Žádné", "ZŠ", "SŠ", "VŠ"]
             }, {
-                "question": [ {
-                    "type": "text",
-                    "QTitle": "Jméno vaší matky?"
-                    }
-                ]
+                "Type": "radiogroup",
+                "CorrectAnsw": "false",
+                "QTitle": "Víte, co jsou to kryptoměny?",
+                "Choices": ["Ano", "Ne", "Nejsem si jistý/á"]
+            }, {
+                "Type": "radiogroup",
+                "CorrectAnsw": "false",
+                "QTitle": "Máte nějaké zkušenosti s investováním do kryptoměn?",
+                "Choices": ["Ano", "Ne a neplánuji to", "Ne, ale přemýšlím o tom"]
+            }, {
+                "Type": "text",
+                "QTitle": "Co Vás od investování do kryptoměn odrazuje?"
+            }, {
+                "Type": "radiogroup",
+                "CorrectAnsw": "false",
+                "QTitle": "Jste vlastníkem nějaké kryptoměny?",
+                "Choices": ["Ano", "Ne"]
+            }, {
+                "Type": "text",
+                "QTitle": "Pokud ano, jakou kryptoměnu vlastníte a proč?"
+            }, {
+                "Type": "checkbox",
+                "CorrectAnsw": "false",
+                "QTitle": "Jaké kryptoměny znáte?",
+                "Choices": ["Bitcoin", "Ethereum", "Dogecoin", "Ripple", "Litecoin", "Tether", "SafeMoon", "Stellar", "NEO"]
+            }, {
+                "Type": "radiogroup",
+                "CorrectAnsw": "false",
+                "QTitle": "Jaký máte postoj ke kryptoměnám?",
+                "Choices": ["Aktivně se o ně zajímám a investuji do nich", "Ne a neplánuji to", "Znám je a zajímám se o ně, ale neinvestuji", "Mám neutrální postoj", "Neznám a nezjímám se", "Nepodporuji je"]
+            }, {
+                "Type": "checkbox",
+                "CorrectAnsw": "false",
+                "QTitle": "Používáte kryptoměny?",
+                "Choices": ["Používám je k placení", "Obchoduji s nimi na burze", "Je to pro mě forma spoření", "Těžím je", "Neznám a nezjímám se", "Nakoupil/a jsem a držím si je", "Nijak je nevyužívám"]
+            }, {
+                "Type": "radiogroup",
+                "CorrectAnsw": "false",
+                "QTitle": "Máte důvěru v trh s kryptoměnami?",
+                "Choices": ["Ano", "Ne", "Nemám na to názor"]
+            }, {
+                "Type": "radiogroup",
+                "CorrectAnsw": "false",
+                "QTitle": "Myslíte, že obchodování s kryptoměnami má potenciál?",
+                "Choices": ["Ano", "Ne", "Nevím"]
+            }, {
+                "Type": "text",
+                "QTitle": "Jaký máte názor na tento způsob výdělku?"
             }
         ]
     },
 
     {
         "info": {
-                "SName": "Testovaci dotaznik02", 
-                "SAuthor": "čet. 2 Volná", 
-                "SAbout": "Dotazník 2", 
-                "QuestSum": "2", 
-                "STime": "10 2"
-            },
-        "pages": [
-            {
-                "question": [ {
-                    "type": "text",
-                    "QTitle": "Vaše hodnost?"
-                    }
-                ]
-            }, {
-                "question": [ {
-                    "type": "text",
-                    "QTitle": "Jméno vaší matky?"
-                    }
-                ]
-            }, {
-                "question": [ {
-                    "type": "radiogroup",
-                    "QTitle": "Kdy jsou Vánoce?",
-                    "Choices": ["12.12.", "24.12.", "1.1."],
-                    "CorrectAnsw": "24.12."
-                    }
-                ]
-            }, {
-                "question": [ {
-                    "type": "checkbox",
-                    "QTitle": "Zvolte mužská jména",
-                    "Choices": ["Radek", "Aneta", "Pavel"],
-                    "CorrectAnsw": ["Radek", "Pavel"]
-                    }
-                ]
-            }
-        ]
-    },
-
-    {
-        "info": {
-                "SName": "Testovaci dotaznik333", 
-                "SAuthor": "čet. J33itka 3", 
-                "SAbout": "3 testovani", 
-                "QuestSum": "3", 
+                "SName": "Sexuální obtěžování ", 
+                "SAuthor": "des. Edita Božková", 
+                "SAbout": "Anonymní dotazník, který má za účelem zjistit míru obětí sexuálního obtěžování", 
+                "QuestSum": "16", 
                 "STime": "10 minut"
             },
-        "pages": [
+        "questions": [
             {
-                "question": [ {
-                    "type": "text",
-                    "QTitle": "Vaše hodnost?"
-                    }
-                ]
+                "Type": "radiogroup",
+                "CorrectAnsw": "false",
+                "QTitle": "Jaké je Vaše pohlaví",
+                "Choices": ["Žena", "Muž"]
             }, {
-                "question": [ {
-                    "type": "text",
-                    "QTitle": "Jméno vaší matky?"
-                    }
-                ]
+                "Type": "text",
+                "QTitle": "Kolik je Vám let?"
             }, {
-                "question": [ {
-                    "type": "radiogroup",
-                    "QTitle": "Kdy jsou Vánoce?",
-                    "Choices": ["12.12.", "24.12.", "1.1."],
-                    "CorrectAnsw": "24.12."
-                    }
-                ]
+                "Type": "radiogroup",
+                "CorrectAnsw": "false",
+                "QTitle": "Jaké je Vaše nejvyšší dosažené vzdělání?",
+                "Choices": ["Žádné", "ZŠ", "SŠ", "VŠ"]
             }, {
-                "question": [ {
-                    "type": "checkbox",
-                    "QTitle": "Zvolte mužská jména",
-                    "Choices": ["Radek", "Aneta", "Pavel"],
-                    "CorrectAnsw": ["Radek", "Pavel"]
-                    }
-                ]
+                "Type": "radiogroup",
+                "CorrectAnsw": "false",
+                "QTitle": "Setkal/a jste se někdy s jakoukoliv formou sexuálního obtěžování?",
+                "Choices": ["Ano", "Ne", "Nejsem si jistý/á"]
+            }, {
+                "Type": "checkbox",
+                "CorrectAnsw": "false",
+                "QTitle": "Pokud ano, s čím přesně?",
+                "Choices": ["Nevyžádané doteky", "Nevhodné poznámky", "Pokřikování na ulici", "Sexualizování Vaší osoby", "Pokus o znásilnění", "Sexuální násilí", "Nevhodné návrhy a nabídky na sexuální služby", "Nežádoucí fotografie"]
+            }, {
+                "Type": "radiogroup",
+                "CorrectAnsw": "false",
+                "QTitle": "Setkal/a jste se někdy s nevhodnými vtipy na Vaši osobu, které měli sexuální podtext?",
+                "Choices": ["Ano", "Ne", "Nejsem si jistý/á"]
+            }, {
+                "Type": "radiogroup",
+                "CorrectAnsw": "false",
+                "QTitle": "Setkal/a jste se někdy s nevhodnými poznámkami či narážkami na Vaši osobu?",
+                "Choices": ["Ano", "Ne", "Nejsem si jistý/á"]
+            }, {
+                "Type": "radiogroup",
+                "CorrectAnsw": "false",
+                "QTitle": "Zažil/a jste někdy nechtěné dotyky na intimní partie?",
+                "Choices": ["Ano", "Ne", "Nejsem si jistý/á"]
+            }, {
+                "Type": "radiogroup",
+                "CorrectAnsw": "false",
+                "QTitle": "Dělal Vám někdo nevhodné sexuální návrhy?",
+                "Choices": ["Ano", "Ne", "Nejsem si jistý/á"]
+            }, {
+                "Type": "radiogroup",
+                "CorrectAnsw": "false",
+                "QTitle": "Stalo se Vám někdy, že Vás někdo sexuálně obtěžoval a Vy jste se cítil/a bezmocně?",
+                "Choices": ["Ano", "Ne", "Nejsem si jistý/á"]
+            }, {
+                "Type": "radiogroup",
+                "CorrectAnsw": "false",
+                "QTitle": "Poslal Vám někdo nevyžádané fotografie svých intimních partií?",
+                "Choices": ["Ano", "Ne", "Nejsem si jistý/á"]
+            }, {
+                "Type": "radiogroup",
+                "CorrectAnsw": "false",
+                "QTitle": "Nutil Vás někdo k sexuálním činnostem?",
+                "Choices": ["Ano", "Ne", "Nejsem si jistý/á"]
+            }, {
+                "Type": "radiogroup",
+                "CorrectAnsw": "false",
+                "QTitle": "Ohradila jste se v případě, že Vás někdo jakkoliv sexuálně obtěžoval?",
+                "Choices": ["Ano", "Ne", "Nejsem si jistý/á"]
+            }, {
+                "Type": "text",
+                "QTitle": "Pokud ne, proč"
+            }, {
+                "Type": "text",
+                "QTitle": "Pokud ano, jak?"
+            }, {
+                "Type": "radiogroup",
+                "CorrectAnsw": "false",
+                "QTitle": "Stali jste se někdy obětí pokusu o znásilnění, či skutečného znásilnění?",
+                "Choices": ["Ano", "Ne", "Nejsem si jistý/á"]
+            }, {
+                "Type": "radiogroup",
+                "CorrectAnsw": "false",
+                "QTitle": "Byl/a jste někdy nahlásit sexuální obtěžování na policii?",
+                "Choices": ["Ano", "Ne, ale chtěl/a jsem", "Nikdy se mi nic takového nestalo", "Ne, nebyl/a"]
+            }, {
+                "Type": "text",
+                "QTitle": "Pokud jste se stali obětí sexuálního násilí a neřešili jste to s policií, jak jste se k tomu postavili?"
+            }, {
+                "Type": "radiogroup",
+                "CorrectAnsw": "false",
+                "QTitle": "Pokud jste se stal/a obětí sexuálního obtěžování, zanechalo to na Vás nějaké následky?",
+                "Choices": ["Nejsem oběť sexuálního násilí", "Nezanechalo to na mně žádné následky", "Ano, zanechalo to na mě trvalé následky"]
             }
         ]
     }
